233188f83b0f4acda6ced09b9588059a3f14eed8
daviidl <david.lema.amado@colexio-karbo.com>
2023-12-13 02:45:21 +0100

    Version 1

--- Documents/NetBeansProjects/Proyecto5/src/main/java/com/mycompany/proyecto5/Proyecto5.java
+++ Documents/NetBeansProjects/Proyecto5/src/main/java/com/mycompany/proyecto5/Proyecto5.java
@@ -11,6 +11,6 @@ package com.mycompany.proyecto5;
 public class Proyecto5 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola Mundo");
     }
 }
